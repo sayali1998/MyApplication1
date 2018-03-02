b4afeec95a5248361f1ffa2ece0ea85e4c7a5f7e
a7r3 <arvindultimate7352@gmail.com>
2018-03-02 15:53:46 +0530

    fragments

---
+++ app/src/main/java/sayalipatil/myapplication/Assignment.java
@@ -0,0 +1,35 @@
+package sayalipatil.myapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Assignment extends Fragment{
+
+
+    public Assignment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_assignment,null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
